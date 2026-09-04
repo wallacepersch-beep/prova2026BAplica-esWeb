c4c4d4cac8c4d203e88c1e6bd78ae951164e449f
WALLACE CASTILLO PERSCH <wallace.persch@acad.univates>
2026-09-03 21:27:18 -0300

    Scripts da pagina

---
+++ cadastro.js
@@ -0,0 +1,7 @@
+botao.addEventListener("click", () => {
+    alert(
+        "Reserva cadastrada com sucesso! Em nome de " + nome + ". Na data de 31 de fevereiro de 2027"
+    );;
+});
+
+const nome = "Manoel Caneta Azul Gomes"
